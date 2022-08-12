5ad210b9958c5e45c89c78cc23b1a4f40c60d421
NIKHIL UPADHYAY <87298477+uniiik@users.noreply.github.com>
2022-08-12 23:39:08 +0530

    Time: 72 ms (49.12%), Space: 27.8 MB (36.37%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
@@ -6,3 +6,4 @@ public:
         return max(nums[n-1]*nums[n-2]*nums[n-3],nums[0]*nums[1]*nums[n-1]);
     }
 };
+//as after sorting it might be possible that negative part number are bigger than positive number so we have just simply taken first 2 negative numbers which will become +ve after multiplication and multiplied it with the last number so that it might become max
