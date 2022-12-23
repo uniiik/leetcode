5d6f223ae7f92b71e7031360e9a7e4ff0a9d45c7
NIKHIL UPADHYAY <87298477+uniiik@users.noreply.github.com>
2022-12-23 15:00:41 +0530

    Time: 462 ms (49.51%), Space: 89 MB (41.83%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -1,19 +1,72 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        int diff=0;
-        int result=0;
-        if(nums.size()<2)
+        
+        int n = nums.size();
+        
+        if(n < 2)
             return 0;
-        else{
         
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++)
+        // find maximum and minimum element of the array
+        
+        int maxi = *max_element(nums.begin(), nums.end());
+        
+        int mini = *min_element(nums.begin(), nums.end());
+        
+        // find the gap
+        
+        int gap = ((maxi - mini) / (n - 1)) + 1;
+        
+        // min_bucket[i] store the min of that bucket
+        
+        vector<int> min_bucket(n - 1, INT_MAX);
+        
+        // max_bucket[i] store the max of that bucket
+        
+        vector<int> max_bucket(n - 1, INT_MIN);
+        
+        // fill the min_bucket and max_bucket
+        
+        for(int i = 0; i < n; i++)
         {
-            diff=nums[i]-nums[i-1];
-            result=max(result,diff);
-        }}
+            if(nums[i] != mini && nums[i] != maxi)
+            {
+                // index of bucket
+                
+                int buk_idx = (nums[i] - mini) / gap;
+                
+                min_bucket[buk_idx] = min(min_bucket[buk_idx], nums[i]);
+                
+                max_bucket[buk_idx] = max(max_bucket[buk_idx], nums[i]);
+            }
+        }
         
-        return result;
+        // now traverse over buckets and find max_gap
+        
+        int max_gap = INT_MIN;
+        
+        // prev store max of prev bucket
+        
+        int prev = mini;
+        
+        // find max gap
+        
+        for(int i = 0; i < n - 1; i++)
+        {
+            // if bucket is not empty
+            
+            if(min_bucket[i] != INT_MAX && max_bucket[i] != INT_MIN)
+            {
+                int curr_gap = min_bucket[i] - prev;
+                
+                max_gap = max(max_gap, curr_gap);
+                
+                prev = max(prev, max_bucket[i]);
+            }
+        }
+        
+        max_gap = max(max_gap, maxi - prev);
+        
+        return max_gap;
     }
 };
