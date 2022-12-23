79e0ed84ddbb003404620c4551f55602cadc9da4
NIKHIL UPADHYAY <87298477+uniiik@users.noreply.github.com>
2022-12-23 13:20:03 +0530

    Time: 17 ms (26.56%), Space: 10.3 MB (49.46%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -20,3 +20,7 @@ public:
         return v1;
     }
 };
+/*
+we have created an unordered set for nums1 array then we have created a variable named key in nums2 array , if the key equals any variable present in the s1 unorderd set then we will remove that element from s1 so that while checking in nums2 if the repetitive element comes in nums2 then it would check in s1 and so it will not be printed so no duplicate element will be printed .
+unorderd set is collecton of elements without duplicate elements
+*/
